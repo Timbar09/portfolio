99be45516a6368003287c405d9e741dec82b0734
Miles Mosweu <tmtmosweu@gmail.com>
2023-10-26 16:26:57 +0200

    Add logo

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,5 +1,13 @@
+import styles from './index.module.css';
+
 const Nav = () => {
-  return <nav>nav</nav>;
+  return (
+    <nav className={styles.nav}>
+      <div className="container">
+        <a href="/">Miles Mosweu</a>
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
