6585411bde3ae599f43596e63e59eb28dfb6a4e7
Miles Mosweu <tmtmosweu@gmail.com>
2023-11-10 21:12:30 +0200

    Import social media icons into component

--- src/components/globals/SocialList.jsx
+++ src/components/globals/SocialList.jsx
@@ -1,5 +1,9 @@
+import { AiFillGithub as Github } from 'react-icons/ai';
+import { AiFillLinkedin as Linkedin } from 'react-icons/ai';
+import { AiFillTwitterCircle as Twitter } from 'react-icons/ai';
+
 const SocialList = () => {
-  return <div>SocialList</div>;
+  return <ul className="social__list"></ul>;
 };
 
 export default SocialList;
