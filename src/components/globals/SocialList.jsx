cdc9827c7e1c1c5b3c8cb29431f013a131058c90
Miles Mosweu <tmtmosweu@gmail.com>
2023-11-10 21:17:29 +0200

    Add socials data

--- src/components/globals/SocialList.jsx
+++ src/components/globals/SocialList.jsx
@@ -2,6 +2,24 @@ import { AiFillGithub as Github } from 'react-icons/ai';
 import { AiFillLinkedin as Linkedin } from 'react-icons/ai';
 import { AiFillTwitterCircle as Twitter } from 'react-icons/ai';
 
+const socialsData = [
+  {
+    id: 1,
+    icon: <Github />,
+    url: 'https://github.com/Timbar09',
+  },
+  {
+    id: 2,
+    icon: <Linkedin />,
+    url: 'www.linkedin.com/in/miles-mosweu09',
+  },
+  {
+    id: 3,
+    icon: <Twitter />,
+    url: 'https://twitter.com/Milez09',
+  },
+];
+
 const SocialList = () => {
   return (
     <ul className="social__list">
