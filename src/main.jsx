609370391f669e9782ebbe187baff25e652369d6
Miles Mosweu <tmtmosweu@gmail.com>
2023-12-16 16:36:00 +0200

    Implement routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
 import App from './App.jsx';
+import Home from './components/home/index.jsx';
+
 import './index.scss';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/page2',
+        element: <h1>Page 2</h1>,
+      },
+      {
+        path: '/page3',
+        element: <h1>Page 3</h1>,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
