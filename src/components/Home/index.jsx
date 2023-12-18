9d4a199e409ae4f4681f3130e2ecbef31a37b0d5
Miles Mosweu <tmtmosweu@gmail.com>
2023-12-18 19:47:42 +0200

    Add home page intro section

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -3,8 +3,14 @@ import { Link } from 'react-router-dom';
 const Home = () => {
   return (
     <div className="home">
-      <h1>Home Page</h1>
       <ul className="home__links">
+        <li className="home__intro">
+          <h1>Miles Mosweu</h1>
+          <p>
+            This is the home page intro section. It is a good place to explain
+            what the site is about and what the user can expect to find here.
+          </p>
+        </li>
         <li className="home__links--item">
           <Link to="/page2">Page 2</Link>
         </li>
