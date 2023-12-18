821f5c9ad3f1c040e71220e11d2e90c390727db7
Miles Mosweu <tmtmosweu@gmail.com>
2023-12-18 20:42:32 +0200

    Add mobile menu and update header styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import { Outlet, useLocation } from 'react-router-dom';
 
 import Nav from './components/Nav';
+import MobileMenu from './components/MobileMenu';
 
 const App = () => {
   const location = useLocation();
 
   return (
     <div className="app">
-      <header>{location.pathname !== '/' && <Nav />}</header>
+      <header>
+        {location.pathname !== '/' && (
+          <>
+            <Nav />
+            <MobileMenu />
+          </>
+        )}
+      </header>
 
       <main>
         <Outlet />
