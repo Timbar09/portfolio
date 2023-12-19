65881cab0f20ad89d5159ca70919fe5a31049e48
Miles Mosweu <tmtmosweu@gmail.com>
2023-12-19 22:55:05 +0200

    Add icons to mobile menu

--- src/components/MobileMenu/index.jsx
+++ src/components/MobileMenu/index.jsx
@@ -1,5 +1,10 @@
 import { Link } from 'react-router-dom';
 import { IoClose as CloseMenuIcon } from 'react-icons/io5';
+import { RiHome3Line as HomeIcon } from 'react-icons/ri';
+import { RiUser3Line as AboutMeIcon } from 'react-icons/ri';
+import { RiFileList2Line as ProjectsIcon } from 'react-icons/ri';
+import { VscTools as CapabilitiesIcon } from 'react-icons/vsc';
+import { RiKakaoTalkLine as ContactIcon } from 'react-icons/ri';
 
 import SocialList from '../globals/SocialList';
 import Logo from '../globals/Logo';
@@ -25,15 +30,38 @@ const MobileMenu = () => {
 
       <ul className="menu__list">
         <li className="menu__item">
-          <Link to="/">Home</Link>
+          <Link to="/" className="flex flex-ai-c gap-1 p-1">
+            <HomeIcon />
+            Home
+          </Link>
         </li>
 
         <li className="menu__item">
-          <Link to="/page2">Page 2</Link>
+          <Link to="/page2">
+            <AboutMeIcon />
+            About Me
+          </Link>
         </li>
 
         <li className="menu__item">
-          <Link to="/page3">Page 3</Link>
+          <Link to="/page3">
+            <ProjectsIcon />
+            Projects
+          </Link>
+        </li>
+
+        <li className="menu__item">
+          <Link to="/page4">
+            <CapabilitiesIcon />
+            Capabilities
+          </Link>
+        </li>
+
+        <li className="menu__item">
+          <Link to="/page5">
+            <ContactIcon />
+            Contact
+          </Link>
         </li>
       </ul>
     </nav>
