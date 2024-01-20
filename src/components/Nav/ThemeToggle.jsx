7d0796b9b7cb30ab4446ad5ba6b889e107c5cb6c
Miles Mosweu <tmtmosweu@gmail.com>
2024-01-20 14:47:14 +0200

    Refactor theme toggle button and add animation

--- src/components/Nav/ThemeToggle.jsx
+++ src/components/Nav/ThemeToggle.jsx
@@ -8,13 +8,27 @@ const ThemeToggle = () => {
   const { theme, toggleTheme } = useContext(ThemeContext);
 
   return (
-    <button className="nav__theme-toggle p-1" title="Toggle dark mode">
-      {theme === 'dark' ? (
-        <LightModeIcon onClick={toggleTheme} />
-      ) : (
-        <DarkModeIcon onClick={toggleTheme} />
-      )}
-    </button>
+    <>
+      <button className="nav__theme p-1" title={`Switch to ${theme} mode`}>
+        {theme === 'dark' ? (
+          <LightModeIcon onClick={toggleTheme} />
+        ) : (
+          <DarkModeIcon onClick={toggleTheme} />
+        )}
+      </button>
+
+      <button
+        className="nav__theme nav__theme-animate flex"
+        title={`Switch to ${theme === 'dark' ? 'light' : 'dark'} mode`}
+      >
+        <span className="darkmode">Dark</span>
+        <span className="lightmode">Light</span>
+        <span className={`slider slider-${theme}mode flex flex-ai-c flex-jc-c`}>
+          {theme === 'dark' ? <DarkModeIcon /> : <LightModeIcon />}
+          <span>{theme.charAt(0).toUpperCase() + theme.slice(1)}</span>
+        </span>
+      </button>
+    </>
   );
 };
 
