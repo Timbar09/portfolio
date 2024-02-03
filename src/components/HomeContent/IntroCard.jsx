0cc1fba538f75e579238bc3aed97ee8c13045f23
Miles Mosweu <tmtmosweu@gmail.com>
2024-02-03 11:01:56 +0200

    Refactor IntroCard component and styles

--- src/components/HomeContent/IntroCard.jsx
+++ src/components/HomeContent/IntroCard.jsx
@@ -1,19 +1,27 @@
+import { Link } from 'react-router-dom';
+
 import './IntroCard.scss';
 
 const IntroCard = () => {
   return (
     <li className="home__intro home__list--item">
       <span className="p-2">
-        <h1>
-          <span className="home__intro--name">
-            <span>Hi👋, I&apos;m</span> <span>Miles</span>.
+        <h1 className="home__intro--title">
+          <span className="home__intro--title__name">
+            <span>Hi👋, I&apos;m</span>{' '}
+            <Link to="/about" className="about-link">
+              Miles
+            </Link>
+            .{' '}
           </span>
-          <span className="home__intro--title">Software Developer</span>
+          <span className="home__intro--title__description">
+            Software Developer
+          </span>
         </h1>
 
-        <p>
-          This is the home page intro section. It is a good place to explain
-          what the site is about and what the user can expect to find here.
+        <p className="home__intro--description">
+          I craft unique, modern websites that captivate and invite engagement,
+          enhancing your business or product&apos;s online presence.
         </p>
       </span>
     </li>
