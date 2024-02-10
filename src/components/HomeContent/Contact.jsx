437cf709f437d0beb30855917803313a97ec2700
Miles Mosweu <tmtmosweu@gmail.com>
2024-02-11 00:16:08 +0200

    Refactor contact section in HomeContent component

--- src/components/HomeContent/Contact.jsx
+++ src/components/HomeContent/Contact.jsx
@@ -15,7 +15,11 @@ const Contact = () => {
         className="flex flex-col flex-jc-sb p-2"
         aria-label="Contact"
       >
-        <h2 className="home__contact--title">Contact Me</h2>
+        <div className="flex flex-jc-sb flex-ai-c">
+          <h2 className="home__contact--title">Contact Me</h2>
+
+          <ArrowForward className="home__contact--link-indicator" />
+        </div>
 
         <div className="home__contact--icon">
           <div className="home__contact--icon__wrap grid grid-pi-c">
@@ -23,14 +27,11 @@ const Contact = () => {
           </div>
         </div>
 
-        <div className="home__contact--link-indicator flex flex-jc-sb flex-ai-c">
-          <SocialList
-            className="social-md flex flex-ai-c gap-1"
-            isNavExpanded={true}
-            isMenuOpen={true}
-          />
-          <ArrowForward />
-        </div>
+        <SocialList
+          className="social-md flex flex-ai-c gap-1"
+          isNavExpanded={true}
+          isMenuOpen={true}
+        />
       </Link>
     </li>
   );
